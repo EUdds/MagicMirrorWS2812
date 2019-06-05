9fddab3b546451acf5db6e1767df28c3a4565f97
Eric <coolspot1000@gmail.com>
2019-06-04 21:05:37 -0500

    updated apis and such

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const handler = new events.EventEmitter();
 
 const NUM_LEDS = 28;
 const PORT = 5000;
+const DEFAULT_BRIGHTNESS = 30;
 let pixelData = new Uint32Array(NUM_LEDS);
 
 console.log(`The party is happening at ${PORT}`);
@@ -14,17 +15,27 @@ http.listen(PORT);
 
 
 LEDS.init(NUM_LEDS);
+LEDS.setBrightness(DEFAULT_BRIGHTNESS);
 console.log('This is where the fun begins');
+
+function shutdown() {
+    console.log('Shutting down Server');
+    http.close();
+    console.log('Server shut down');
+    console.log('Resetting LEDs');
+    LEDS.reset();
+    console.log('LEDs reset');
+    console.log('Popping off like some pop off backstroke');
+    process.exit(0);
+}
 
 // Clear all LEDS before exiting program
 process.on('SIGINT', () => {
-    LEDS.reset();
-    process.nextTick(() => { process.exit(0);});
+    shutdown();
 });
 
 process.on('exit', () => {
-    LEDS.reset();
-    process.nextTick(() => { process.exit(0);});
+    shutdown();
 });
 
 // LED Related Functions
@@ -33,17 +44,13 @@ function rgb2Int(r, g, b) {
     return ((r & 0xff) << 16) + ((g & 0xff) << 8) + ((b & 0xff));
 }
 
-function setLed(id, r, g , b, brightness) {
-    LEDS.setBrightness(brightness);
+function setLed(id, r, g , b) {
     let color = rgb2Int(r,g,b);
     pixelData[id] = color;
     LEDS.render(pixelData);
 }
 
-function setRange(from, to, r, g , b, brightness) {
-    console.log('Range time');
-    LEDS.setBrightness(brightness);
-    console.log('Brightness set');
+function setRange(from, to, r, g , b) {
     let color = rgb2Int(r,g,b);
     console.log('color set');
     for (let i = from; i<= to; i++) {
@@ -54,6 +61,9 @@ function setRange(from, to, r, g , b, brightness) {
     console.log('Lets blow this popsicle stand');
  }
 
+function setBrightness(brightness) {
+    LEDS.setBrightness(brightness);
+}
 // Status Updates
 function sendGoodStatus(req, res) {
     res.type('application/json');
@@ -71,22 +81,43 @@ app.get('/test', (req, res) => {
     console.log('Said Goodbye');
 });
 
+app.get('/setBrightness', (req, res) => {
+    let b = req.query.value;
+    LEDS.setBrightness(b);
+    sendGoodStaatus(req, res);
+});
+
 app.get('/setLed', (req, res) => {
     let id = parseInt(req.query.id);
     let r = req.query.r;
     let g = req.query.g;
     let b = req.query.b;
-    let brightness = req.query.bright;
     if (!id) {
         console.log('Who do you know here?');
-        return
+        sendGoodStatus(req,res);
+        return;
     }
-    console.log(`Setting LED ${id} to RGB ${r} ${g} ${b} with a brightness of ${brightness}`);
-    setLed(id,r,g,b,brightness);
+    console.log(`Setting LED ${id} to RGB ${r} ${g} ${b}`);
+    setLed(id,r,g,b);
     sendGoodStatus(req, res);
 });
 
 app.get('/reset', (req, res) => {
     setRange(0,27,0,0,0,30);
     sendGoodStatus(req,res);
+});
+
+app.get('/setRange', (req, res) => {
+    let from = req.query.from;
+    let to = req.query.to;
+    let r = req.query.r;
+    let g = req.query.g;
+    let b = req.query.b;
+    if (from < 0 || to >= NUM_LEDS) {
+        console.log('Wrong housue dude');
+        sendGoodStatus(req, res);
+        return;
+    }
+    setRange(from,to,r,g,b);
+    sendGoodStatus(req, res);
 });
